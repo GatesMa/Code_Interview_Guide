8939fd63a7cb749c287e53c995a9161e774db284
gatesma <gatesma@foxmail.com>
2020-06-08 09:37:49 +0800

    Add Solutions.

---
+++ src/Chapter1/C2/Main.java
@@ -0,0 +1,12 @@
+package Chapter1.C2;
+
+/**
+ * Copyright (C), 2020
+ * FileName: Main
+ * Author:   Marlon
+ * Email: gatesma@foxmail.com
+ * Date:     2020/6/8 09:37
+ * Description:
+ */
+public class Main {
+}
